7815331558b033395297c96079ca68cbbbeda129
Thrishika R <152395737+thrishi0610@users.noreply.github.com>
2025-07-01 14:42:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         try {
             FileWriter writer = new FileWriter("output.html");
-            writer.write("<h1>Hello, World from Docker</h1>");
+            writer.write("<h1>Hello, World from Docker6</h1>");
             writer.close();
             System.out.println("HTML file created");
         } catch (IOException e) {
